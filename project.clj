c3393887e39f660b91010263dd8dd21d1dcbbaaf
marchrock <m4rchrock@gmail.com>
2017-04-12 02:07:55 +0900

    add minimal tomcat adapter

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.6.0-RC2"]
+                 [ring/ring-servlet "1.6.0-RC2"]
                  [org.apache.tomcat.embed/tomcat-embed-core "8.5.13"]])
--- src/ring/adapter/tomcat.clj
+++ src/ring/adapter/tomcat.clj
@@ -1,1 +1,26 @@
-(ns ring.adapter.tomcat)
+(ns ring.adapter.tomcat
+  "Ring adapter for Apache Tomcat"
+  (:import [org.apache.catalina.startup Tomcat])
+  (:require [ring.util.servlet :as ring-servlet]))
+
+(defn- create-server [options]
+  (let [tomcat (doto (Tomcat.)
+                 (.setPort (:port options 8080))
+                 (.setBaseDir "."))
+        server (.getServer tomcat)
+        host (.getHost tomcat)]
+    (.setAppBase host "resources")
+    tomcat))
+
+(defn run-tomcat
+  "Start a Tomcat to serve given handler with supplied options:
+
+  :port - the port to listen on (default: 8080)"
+  [handler options]
+  (let [server (create-server options)
+        context (.addContext server "" ".")]
+    (.addServlet server "" "ring-servlet" (ring-servlet/servlet handler))
+    (.addServletMapping context "/*" "ring-servlet")
+    (.start server)
+    (.await (.getServer server))
+    server))
