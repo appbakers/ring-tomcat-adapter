4ea2c492fa4e95e3b4454c4006000d618d62cdb3
marchrock <m4rchrock@gmail.com>
2017-04-12 20:40:07 +0900

    bumped version number to 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,12 +1,8 @@
-(defproject ring-tomcat-adapter :lein-v
+(defproject ring-tomcat-adapter "0.1.5-SNAPSHOT"
   :description "Ring Apache Tomcat adapter"
   :url "http://github.com/marchrock/ring-tomcat-adapter"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :release-tasks [["vcs" "assert-committed"]
-                  ["v" "update"]
-                  ["vcs" "push"]
-                  ["deploy" "clojars"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.6.0-RC2"]
                  [ring/ring-servlet "1.6.0-RC2"]
