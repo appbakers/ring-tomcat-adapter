307bec17107ce0a51bea54121dac7f0a5a60e0f1
marchrock <m4rchrock@gmail.com>
2017-08-10 11:00:45 +0900

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marchrock/ring-tomcat-adapter "0.4.3"
+(defproject marchrock/ring-tomcat-adapter "0.4.4"
   :description "Ring Apache Tomcat adapter"
   :url "http://github.com/marchrock/ring-tomcat-adapter"
   :license {:name "Apache License, Version 2.0"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.6.2"]
                  [ring/ring-servlet "1.6.2"]
-                 [org.apache.tomcat.embed/tomcat-embed-core "8.5.16"]]
+                 [org.apache.tomcat.embed/tomcat-embed-core "8.5.20"]]
 
   :profiles
   {:dev {:dependencies [[clj-http "3.5.0"]]}
